1e2b3d5c1e942261406ff2c44e92270d795eeb0e
DiptiRagiwar <95093874+DiptiRagiwar@users.noreply.github.com>
2021-12-19 14:58:37 +0530

    Add files via upload

---
+++ Anudip Assignments/Exercise1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    class Exercise1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
