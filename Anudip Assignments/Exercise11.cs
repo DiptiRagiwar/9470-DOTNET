d19987743693adba5522336aef574d8339957050
DiptiRagiwar <95093874+DiptiRagiwar@users.noreply.github.com>
2021-12-19 15:05:09 +0530

    Add files via upload

---
+++ Anudip Assignments/Exercise11.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { int=n
+            if (n < 0)
+            {
+                Console.WriteLine("Negative Number!");
+            }
+            else if (n == 0)
+            {
+                Console.WriteLine("Zero");
+            }
+            else
+            {
+                Console.WriteLine("Positive Number!");
+            }
+
+        }
+    }
