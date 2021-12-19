12b6ad013d556be7735c876869b27c6163e61372
DiptiRagiwar <95093874+DiptiRagiwar@users.noreply.github.com>
2021-12-19 15:09:43 +0530

    Add files via upload

---
+++ Anudip Assignments/single-dimension-arr.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Array_single_dimensional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 10, 15, 20, 100 };
+            int len = arr.Length;
+            Console.WriteLine("Length of Array " + len);
+            Console.WriteLine(arr[0] + " " + arr[4]);
+        }
+    }
+}
