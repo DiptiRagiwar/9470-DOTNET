bad3a26ce1d56e4d0070feab2db6fef30fcca4e1
DiptiRagiwar <95093874+DiptiRagiwar@users.noreply.github.com>
2021-12-19 15:00:29 +0530

    Add files via upload

---
+++ Anudip Assignments/Exercise4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(-5+6*2);
+            //-5+12=7
+            Console.WriteLine((35 + 5) % 7);
+            //40%7=5 (remainder of 40/7)
+            Console.WriteLine(14 + -4 * 6 / 11);
+            //14 - (24/11)= 14 - 2 = 12
+        }
+    }
+}
