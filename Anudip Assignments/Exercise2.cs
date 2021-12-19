33feaac13fd944dcbc6ef2c9c44dcb4f7cb4af23
DiptiRagiwar <95093874+DiptiRagiwar@users.noreply.github.com>
2021-12-19 14:59:12 +0530

    Add files via upload

---
+++ Anudip Assignments/Exercise2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(22+22);
+            Console.WriteLine(22 - 22);
+        }
+    }
+}
