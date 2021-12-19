3786e7e9a4f0f762e6095f3de573a80b54d60989
DiptiRagiwar <95093874+DiptiRagiwar@users.noreply.github.com>
2021-12-19 15:13:37 +0530

    Add files via upload

---
+++ Anudip Assignments/Dynamic array.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Dynamic_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        class Demo
+        {
+            dynamic Data = 12;
+            public int Method(int A, int B)
+            {
+                return (A + B) * Data;
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            { DemoObj = newDemo();
+                dynamic value1 = 10;
+                dynamic value2 = 11;
+                dynamic Str = "Your Result Is: ";
+                Console.WriteLine(Str + Obj.Method(value1, value2));
+                Console.ReadLine();
+            }
+        }
+        
+    }
+        
+    
+}
