e383b26ee1b9f1a0429745e12d1aa16c79496464
DiptiRagiwar <95093874+DiptiRagiwar@users.noreply.github.com>
2021-12-19 15:07:30 +0530

    Add files via upload

---
+++ Anudip Assignments/Exercise13.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercise13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the amount of celcius");
+            int celcius = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine( "Kelvin = {0} ", celcius + 32);
+            Console.WriteLine( "Fahrenhit = {0}", celcius*18 /10+32 );
+        }
+    }
+}
