66d40c6234c54cf5ebdb41a012c395086f6edf45
DiptiRagiwar <95093874+DiptiRagiwar@users.noreply.github.com>
2021-12-19 15:06:16 +0530

    Add files via upload

---
+++ Anudip Assignments/exercise12.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num < 0 || num > 15)
+            {
+                Console.WriteLine("Wrong number");
+            }
+            else if(num >=0 && num < 10)
+            {
+                Console.WriteLine("false");
+            }
+            else
+            {
+                Console.WriteLine("Answer is not Correct");
+            }
+            Console.ReadLine();
+        }
+    }
+}
