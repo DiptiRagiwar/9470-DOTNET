ac3561fd6f46fb0655dcf8a98b7953374ac125d5
DiptiRagiwar <95093874+DiptiRagiwar@users.noreply.github.com>
2021-12-19 15:12:28 +0530

    Add files via upload

---
+++ Anudip Assignments/Local array.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Local_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 20;
+            var s = "javatpoint";
+            var arr = new[] { 1, 2, 3 };
+            Console.WriteLine(a);
+            Console.WriteLine(s);
+            Console.WriteLine(arr[2]);
+
+        }
+    }
+}
