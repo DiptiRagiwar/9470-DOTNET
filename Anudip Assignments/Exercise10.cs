821fe50e031133305647d65eb0e03daf4c65ea19
DiptiRagiwar <95093874+DiptiRagiwar@users.noreply.github.com>
2021-12-19 15:04:32 +0530

    Add files via upload

---
+++ Anudip Assignments/Exercise10.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise_10_if_else_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter a number to check : ");
+            num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+               {
+                Console.WriteLine("It Is Even Number");
+            }
+            else
+            {
+                Console.WriteLine("It Is Odd Number");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
