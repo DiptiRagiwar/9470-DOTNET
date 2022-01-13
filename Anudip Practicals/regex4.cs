3f829bdcc7ac69bb3741d83ab70b5893358c1adc
DiptiRagiwar <95093874+DiptiRagiwar@users.noreply.github.com>
2022-01-13 13:57:06 +0530

    Add files via upload

---
+++ Anudip Practicals/regex4.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+using System;
+namespace RegularExpressionsSample
+{
+    class regex4
+    {
+        static void Main(string[] args)
+        {
+            string regularExpression = @"^(?=.?[A - Z])(?=.?[a - z])(?=.?[0 - 9])(?=.?[#?!@$%^&*-]).{8,}$";
+            Console.WriteLine("Please input password for check:");
+            string inputString = Console.ReadLine();
+            if (Regex.IsMatch(inputString, regularExpression))
+            {
+                Console.WriteLine("It's correct security password");
+            }
+            else
+            {
+                Console.WriteLine("It's incorrect Security Password.");
+            }
+            Console.Read();
+        }
+    }
+}
