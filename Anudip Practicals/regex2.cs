51f4cf09f04b85abad885596a9631cb85134f8f9
DiptiRagiwar <95093874+DiptiRagiwar@users.noreply.github.com>
2022-01-13 13:37:54 +0530

    Add files via upload

---
+++ Anudip Practicals/regex2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegularExpression2
+{
+    class regex2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string email = "diptiragiwar@gmail.com";
+            var result = Regex.IsMatch(email, @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+            Console.Write("is valid:{0}", result);
+            Console.ReadLine();
+        }
+    }
+}
