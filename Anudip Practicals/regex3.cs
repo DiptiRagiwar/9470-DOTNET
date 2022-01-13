075813f84ac772d0cf8ac6fbd5e138468dfe3d16
DiptiRagiwar <95093874+DiptiRagiwar@users.noreply.github.com>
2022-01-13 13:57:40 +0530

    Add files via upload

---
+++ Anudip Practicals/regex3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegularExpression2
+{
+    class regex3
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hi,welcome@DotNet.com";
+
+            string result = Regex.Replace(str, "[^a-zA-Z0-9_]+", " ");
+            Console.Write("{0}", result);
+            Console.ReadLine();
+        }
+    }
+}
